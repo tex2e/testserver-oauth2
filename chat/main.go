3d82e2659593964295b4004ba8cdbdce5097959c
Mako <mnfeconicu41@gmail.com>
2019-06-03 18:41:26 +0900

    http server with template.html

--- chat/main.go
+++ chat/main.go
@@ -4,21 +4,27 @@ package main
 import (
     "log"
     "net/http"
+    "text/template"
+    "path/filepath"
+    "sync"
 )
 
-func main()  {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte(`
-            <html>
-                <head>
-                    <title>Chat</title>
-                </head>
-                <body>
-                    Let's chat!
-                </body>
-            </html>
-        `))
+type templateHandler struct {
+    once     sync.Once
+    filename string
+    templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    t.once.Do(func () {
+        t.templ = template.Must(template.ParseFiles(
+            filepath.Join("templates", t.filename)))
     })
+    t.templ.Execute(w, nil)
+}
+
+func main()  {
+    http.Handle("/", &templateHandler{filename: "chat.html"})
 
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
